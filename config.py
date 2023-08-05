a7759b487c3aada6713f77cf5542bb0762a8215a
EgorNifakin <63055406+Egor-S-N@users.noreply.github.com>
2023-08-05 16:04:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ BOT_TOKEN = '6371141203:AAGIy1da4czNffhhs5sKzdavQlEHtst8UCI'
 AMPLITUDE_API_KEY = 'ada61f3eec2f28002c5685fd91f77446'
 AMPLITUDE_API_URL = 'https://api2.amplitude.com/2/httpapi'
 
-# host = '127.0.0.1'
-# user = 'postgres'
-# password = 'qwerty'
-# db_name = 'database'
-
-host = 'telegram-bot-ai-enifakin.amvera.io'
-user = 'ENifakin'
-password = '123123En'
+host = 'telegram-bot-ai.onrender.com'
+user = 'postgres'
+password = 'qwerty'
 db_name = 'database'
+
+# host = 'telegram-bot-ai-enifakin.amvera.io'
+# user = 'ENifakin'
+# password = '123123En'
+# db_name = 'database'
 port = 5432
