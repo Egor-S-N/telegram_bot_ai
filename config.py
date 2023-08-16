7784c9ef10aebcb0cde1b9a24ad239959e8a2fb7
EgorNifakin <63055406+Egor-S-N@users.noreply.github.com>
2023-08-16 11:05:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,11 @@
-BOT_TOKEN = '1627292482:AAGS5nUNtCkluRQBBKi6JWXBrO9-34gOEyE'
+BOT_TOKEN = ''
 
-AMPLITUDE_API_KEY = 'ada61f3eec2f28002c5685fd91f77446'
-AMPLITUDE_API_URL = 'https://api2.amplitude.com/2/httpapi'
+AMPLITUDE_API_KEY = ''
+AMPLITUDE_API_URL = ''
 
-host = 'telegram-bot-ai.onrender.com'
+host = ''
 user = 'postgres'
 password = 'qwerty'
 db_name = 'database'
 
-# host = 'telegram-bot-ai-enifakin.amvera.io'
-# user = 'ENifakin'
-# password = '123123En'
-# db_name = 'database'
 port = 80
